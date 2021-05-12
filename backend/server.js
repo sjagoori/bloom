3360598194cd82d1a3cebbc725fb01971fddc055
sjagoori <sjagoori@users.noreply.github.com>
2021-05-12 18:04:25 +0200

    + docs

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,17 @@ app.use(express.json()).use(express.urlencoded({ extended: true })).use(cors());
 app.get("/hello", (req, res) =>
   res.json({ status: 200, body: "Hello World!" })
 );
-
+/**
+ * @title What the do
+ * @description login logica
+ * @TODO login logica, check if user exists in db, if so return true, sign in whatever, bake some cookies
+ */
 app.post("/test", (req, res) => {
   console.log(req.body);
+
+  // login using req.body
+  
+  //return cookie data
 
   return res.json({ foo: "bar " });
 });
