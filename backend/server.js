bba9bf9b08a3d78538575b38af67bde269f4e1f3
sjagoori <sjagoori@users.noreply.github.com>
2021-05-12 14:01:29 +0200

    + backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const port = process.env.PORT || 3000,
+  express = require("express"),
+  app = express(),
+  http = require("http").createServer(app),
+  io = require("socket.io")(http);
+
+app.use(express.json()).use(express.urlencoded({ extended: false }));
+
+app.get('/hello', (res, req) => res.json({status: 200, body: 'Hello World!'}))
+
+app.listen(port, () => console.log(`listening to port ${port}`));
