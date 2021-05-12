c1a119f989cfa7dda618a23daab1c814b175fdc7
sjagoori <sjagoori@users.noreply.github.com>
2021-05-12 16:59:52 +0200

    + server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 
-const port = process.env.PORT || 3000,
+const port = process.env.PORT || 3001,
   express = require("express"),
   app = express(),
+  cors = require("cors"),
   http = require("http").createServer(app),
   io = require("socket.io")(http);
 
-app.use(express.json()).use(express.urlencoded({ extended: false }));
+app.use(express.json()).use(express.urlencoded({ extended: false })).use(cors);
 
-app.get('/hello', (res, req) => res.json({status: 200, body: 'Hello World!'}))
-
+app.get('/hello', (req, res) => res.json({status: 200, body: 'Hello World!'}))
+app.post('/test', (req, res) => {
+  console.log("test");
+  return res.status(200).json({ some: "response" });
+})
 app.listen(port, () => console.log(`listening to port ${port}`));
