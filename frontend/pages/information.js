bdb207d39e5dae995dc26ec15d73bacf08df568d
dewarian <nathanbommezijn@gmail.com>
2021-05-16 15:02:17 +0200

    ~ Update navigation

--- frontend/pages/information.js
+++ frontend/pages/information.js
@@ -1,17 +1,17 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Header from "../components/header/Header";
 import styles from "../styles/information.module.css";
 
 export default function Blog() {
+  const router = useRouter()
 
   return (
     <>
     <Header name="Thema's"/>
-    <Link href="/blog">
-    <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <button type="button" onClick={() => router.back()}>    <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-    </svg>
-    </Link>
+    </svg></button>
+
     <ul>
       <li className={styles.accordion}>Hoe werkt bloom? <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
