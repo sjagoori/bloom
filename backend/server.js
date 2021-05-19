53546277076551c16e4834aeca980b1d2fd96d78
sjagoori <sjagoori@users.noreply.github.com>
2021-05-19 10:36:18 +0200

    dev branch

--- backend/server.js
+++ backend/server.js
@@ -10,4 +10,4 @@ app.use(express.json()).use(express.urlencoded({ extended: false }));
 
 app.get('/hello', (res, req) => res.json({status: 200, body: 'Hello World!'}))
 
-app.listen(port, () => console.log(`listening to port ${port}`));
+app.listen(port, () => console.log(`listening to port ${port}`))
