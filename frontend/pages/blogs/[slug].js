2754ed42444ca307dfeed7de49015256218cf240
dewarian <nathanbommezijn@gmail.com>
2021-05-22 14:43:58 +0200

    + FIX wack merge gave wack shit
    Co-authored-by: Shabier <sjagoori@users.noreply.github.com>

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -11,25 +11,25 @@ export default function Blog({ data }) {
   const header = <header>
     <img className={styles.coverImage} src={`/images/thema-${slug}.jpg`} />
     {Object.values(data.header).map((key, index) => {
-      return typeof key.element != 'img' ? React.createElement(`${key.element}`, { key: index }, `${key.content}`) : <Image src={key.link}></Image>
+      return typeof key.element != 'img' ? React.createElement(`${key.element}`, { key: index, className: `${styles[`${key.class}`]}` }, `${key.content}`) : <Image src={key.link}></Image>
     })}
   </header>
 
   const klachten = <main>
     {Object.values(data.klachten).map((key, index) => {
-      return key.element != 'ul' ? React.createElement(`${key.element}`, { key: index, className: `${key.class}` }, `${key.content}`) : <ul>{key.content.map((element, i) => <li key={i}>{element}</li>)}</ul>
+      return key.element != 'ul' ? React.createElement(`${key.element}`, { key: index, className: `${styles[`${key.class}`]}` }, `${key.content}`) : <ul>{key.content.map((element, i) => <li key={i}>{element}</li>)}</ul>
     })}
   </main>
 
   const tips = <main>
     {Object.values(data.tips).map((key, index) => {
-      return key.element != 'ul' ? React.createElement(`${key.element}`, { key: index, className: `${key.class}` }, `${key.content}`) : <ul>{key.content.map((element, i) => <li key={i}>{element}</li>)}</ul>
+      return key.element != 'ul' ? React.createElement(`${key.element}`, { key: index, className: `${styles[`${key.class}`]}` }, `${key.content}`) : <ul>{key.content.map((element, i) => <li key={i}>{element}</li>)}</ul>
     })}
   </main>
 
   const hulp = <main>
     {Object.values(data.hulp).map((key, index) => {
-      return key.element != 'ul' ? React.createElement(`${key.element}`, { key: index, className: `${key.class}` }, `${key.content}`) : <ul>{key.content.map((element, i) => <li key={i}>{element}</li>)}</ul>
+      return key.element != 'ul' ? React.createElement(`${key.element}`, { key: index, className: `${styles[`${key.class}`]}` }, `${key.content}`) : <ul>{key.content.map((element, i) => <li key={i}>{element}</li>)}</ul>
     })}
   </main>
 
@@ -75,5 +75,6 @@ export default function Blog({ data }) {
 export async function getServerSideProps({ params }) {
   const res = await fetch(`http://localhost:3001/blog/${params.slug}`)
   const data = await res.json()
-  return { props: { data } }
+
+  return { props: {data}}
 }
