e92190cf65301cfdf0eaedf0f46ed4fd436a2a9b
dewarian <nathanbommezijn@gmail.com>
2021-05-22 23:42:08 +0200

    ~ Update header component

--- frontend/components/header/Header.js
+++ frontend/components/header/Header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styles from "./Header.module.css";
 
 export default function Header(props) {
+
   return (
     <>
     <div className={styles.container}>
@@ -9,6 +10,7 @@ export default function Header(props) {
       <Link href="/information">
         <img src="/icons/informatie-icoon.svg" alt="information symbol" className={`${styles.icon}`} />
       </Link>
+      <img src="/icons/zoek-icoon.svg" alt="" className={`${styles.icon} ${styles.search}`}/>
     </div>
     </>
   )
