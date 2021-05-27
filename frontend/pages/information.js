d050a1e2980e336a3e12808e818427aa68c90cdc
dewarian <nathanbommezijn@gmail.com>
2021-05-27 13:42:21 +0200

    ~ Setup information page

--- frontend/pages/information.js
+++ frontend/pages/information.js
@@ -1,28 +1,64 @@
 import { useRouter } from 'next/router';
-import Header from "../components/header/Header";
-import styles from "../styles/information.module.css";
+import styles from '../styles/information.module.css';
 
-export default function Blog() {
+const frequentlyAsked = [
+  {
+    question: 'Hoe werkt Bloom?',
+    answer: 'Bloom werkt op een mobiele apparaat met internet.'
+  },
+  {
+    question: 'Hoe is Bloom ontstaan?',
+    answer: 'Bloom werkt op een mobiele apparaat met internet.'
+  },
+  {
+    question: 'Hulpaanbieders',
+    answer: 'Bloom werkt op een mobiele apparaat met internet.'
+  },
+  {
+    question: 'Veelgestelde vragen',
+    answer: 'Bloom werkt op een mobiele apparaat met internet.'
+  },
+  {
+    question: 'Gegevensbeleid',
+    answer: 'Bloom werkt op een mobiele apparaat met internet.'
+  },
+  {
+    question: 'Gebruikersvoorwaarden',
+    answer: 'Je mag het gebruiken als je geen mensen pest.'
+  },
+  {
+    question: 'Een probleem rapporteren',
+    answer: 'but Why'
+  }
+]
+
+// const details = (data) => {
+//   return <details>
+//     <summary>hehehe</summary>
+//     {data.map((element, key) => {
+//       console.log(element);
+//       <p key={key}>{element.question}</p>
+//     })}
+//   </details>
+// }
+
+export default function Information() {
   const router = useRouter()
 
   return (
     <>
-    <Header name="Thema's"/>
-    <button type="button" onClick={() => router.back()}>    <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-    </svg></button>
-
-    <ul>
-      <li className={styles.accordion}>Hoe werkt bloom? <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-</svg></li>
-      <li className={styles.accordion}>Hoe is bloom ontstaan?</li>
-      <li className={styles.accordion}>Hulpaanbieders</li>
-      <li className={styles.accordion}>Veelgestelde vragen</li>
-      <li className={styles.accordion}>Gegevensbeleid</li>
-      <li className={styles.accordion}>Gebruikersvoorwaarden</li>
-      <li className={styles.accordion}>Een probleem rapporteren</li>
-    </ul>
+      <div className={styles.topBar}>
+        <button type="button" onClick={() => router.back()}>
+          <img src="/icons/chevron-icoon.svg" alt=">" />
+        </button>
+        <h1>bloom</h1>
+      </div>
+    {Object.keys(frequentlyAsked).map((element, key) => {
+      return <details className={styles.information} key={key}>
+        <summary>{frequentlyAsked[element].question}</summary>
+        <p>{frequentlyAsked[element].answer}</p>
+      </details>
+    })}
     </>
   );
 }
