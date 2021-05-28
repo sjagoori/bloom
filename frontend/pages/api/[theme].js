665d921434259f4e033ed641e21e9aa5bbe69279
dewarian <nathanbommezijn@gmail.com>
2021-05-28 15:24:41 +0200

    + Modular theme API

--- frontend/pages/api/[theme].js
+++ frontend/pages/api/[theme].js
@@ -1,18 +1,19 @@
-// import * as themes from "../_files/blogContent.json";
-const themes = require('./_files/blogContent.json')
+const geld = require('./_files/geld.json')
+const angst = require('./_files/angst.json')
+const lichaam = require('./_files/lichaam.json')
+const relatie = require('./_files/relatie.json')
+const somberheid = require('./_files/somberheid.json')
+const vermoeidheid = require('./_files/vermoeidheid.json')
 
-export default function themeHandler(req, res) {
-  const {theme} = req.query
-  const string = `themes.${theme}`
-  console.log(JSON.stringify(`${themes}.${theme}`))
-  res.status(200).json(themes.vermoeidheid)
-  // filter.length > 0 ? res.status(200).json(filter[0]) : res.status(404).json({message: `What you wanted (${slug}) is nowhere to be seen.`})
-  // res.end(`theme: ${themes}.${res.query}`)
-
+const dataset = {
+  angst: {data: angst},
+  geld: {data: geld},
+  lichaam: {data: lichaam},
+  relatie: {data: relatie},
+  somberheid: {data: somberheid},
+  vermoeidheid: {data: vermoeidheid}
 }
 
-
-// export function handler(req, res) {
-//   const { slug } = req.query
-//   res.end(`Post: ${slug.join(', ')}`)
-// }
+export default function themeHandler(req, res) {
+  (Object.keys(dataset).filter(key => key === req.query.theme).length > 0) ? res.status(200).json(dataset[req.query.theme].data) : res.status(400).json({message: 'No blogpost found'})
+}
