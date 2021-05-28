b75eab26ecee6456d132faac7d89ff5be2a25521
dewarian <nathanbommezijn@gmail.com>
2021-05-28 22:35:29 +0200

    + #14 Create API for blog content
    Theme content is now populated via nextjs API routes. Content can be dynamically updated or added uponn by creating new JSON files

--- frontend/pages/api/[theme].js
+++ frontend/pages/api/[theme].js
@@ -1,19 +1,9 @@
-const geld = require('./_files/geld.json')
-const angst = require('./_files/angst.json')
-const lichaam = require('./_files/lichaam.json')
-const relatie = require('./_files/relatie.json')
-const somberheid = require('./_files/somberheid.json')
-const vermoeidheid = require('./_files/vermoeidheid.json')
-
-const dataset = {
-  angst: {data: angst},
-  geld: {data: geld},
-  lichaam: {data: lichaam},
-  relatie: {data: relatie},
-  somberheid: {data: somberheid},
-  vermoeidheid: {data: vermoeidheid}
-}
+const fs = require('fs')
 
 export default function themeHandler(req, res) {
-  (Object.keys(dataset).filter(key => key === req.query.theme).length > 0) ? res.status(200).json(dataset[req.query.theme].data) : res.status(400).json({message: 'No blogpost found'})
+    try {
+    return res.status(200).json(JSON.parse(fs.readFileSync(`./pages/api/_files/${req.query.theme}.json`, 'utf-8')))
+  } catch (error) {
+    return res.status(400).json({msg: "*instense confusion stare*"})
+  }
 }
