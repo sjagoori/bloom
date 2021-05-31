79f1012e1efc93302ba2c422ebf214a6a85817b1
sjagoori <sjagoori@users.noreply.github.com>
2021-05-31 21:57:55 +0200

    ~ formatting

--- frontend/helpers/parseCookie.js
+++ frontend/helpers/parseCookie.js
@@ -5,11 +5,11 @@
  * @source https://www.30secondsofcode.org/js/s/parse-cookie
  * @TODO move somewhere else
  */
-exports.parseCookie = (str) => {
-  str.split(";")
+exports.parseCookie = (str) =>
+  str
+    .split(";")
     .map((v) => v.split("="))
     .reduce((acc, v) => {
       acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
       return acc;
     }, {});
-}
