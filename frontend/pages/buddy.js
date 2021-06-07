533c70992a031ca8ff1841292b8c52adbf416e85
dewarian <nathanbommezijn@gmail.com>
2021-06-07 23:38:24 +0200

    [WIP] Start filter overlay

--- frontend/pages/buddy.js
+++ frontend/pages/buddy.js
@@ -1,16 +1,33 @@
-import BuddyList from '@/components/buddyList/BuddyList';
+// import BuddyList from '@/components/buddyList/BuddyList';
+import { useState } from 'react';
 import styles from '../styles/Buddy.module.css';
 
-
 export default function Buddy() {
-  
+  const [modalVisible, setModalVisible] = useState(false)
   return (
     <>
     <header className={styles.topBar}>
     <h1>Buddy zoeken</h1>
-    <button><img src="./icons/filter-icoon.svg" alt="Filter" /></button>
+    <button
+      onClick={() => setModalVisible()}  
+    ><img src="./icons/filter-icoon.svg" alt="Filter" /></button>
     </header>
-    <BuddyList />
+    {/* <BuddyList /> */}
+    <main className={styles.filterContainer}>
+      <section>
+        <p className={styles.leadingText}>ik wil contact met...</p>
+        <form action="" method="post">
+          <input type="checkbox" name="sameTypeCancer" id="" />
+          <label htmlFor="sameTypeCancer">Gebruikers met hetzelfde type kanker</label>
+          <input type="checkbox" name="usersOfGender" id="" />
+          <label htmlFor="usersOfGender">Gebruikers van een bepaald gender</label>
+          <input type="checkbox" name="typeOfGender" id="" disabled/>
+          <input type="checkbox" name="typeOfGender" id="" disabled/>
+          <input type="checkbox" name="typeOfGender" id="" disabled/>
+          
+        </form>
+      </section>  
+    </main> 
     </>
   )
 }
