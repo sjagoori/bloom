e3e9a0a2b88d986e23b053bc4d733569ab276079
dewarian <nathanbommezijn@gmail.com>
2021-06-09 10:10:54 +0200

    ~ Move data retrieval to parent component

--- frontend/pages/buddies.js
+++ frontend/pages/buddies.js
@@ -7,7 +7,20 @@ const filter = {
   
 }
 
-export default function Buddies() {
+const userData = [
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Hazar Car Cybertruck the third', kankerType: ['breinkanker','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'nathan Bommezijn', kankerType: ['breinkanker','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Shabier', kankerType: ['prolife','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Allyssa Alimoestar', kankerType: ['testlife','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Justus sturkenboom', kankerType: ['testlife','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Robert Spier', kankerType: ['testlife','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Janno Kaprisitias', kankerType: ['testlife','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Joost Faber', kankerType: ['testing','pain'], birthDate: '1995-06-05'},
+  {_id: '60b8e9c2aea08a5f61999c19' ,name: 'Allyssa', kankerType: ['testing','pain'], birthDate: '1995-06-05'}
+]
+
+export default function Buddies({data}) {
+  // data = userData
   // Show or hide filters
   const [modalVisible, setModalVisible] = useState(false)
   return (
@@ -18,7 +31,7 @@ export default function Buddies() {
       onClick={() => setModalVisible()}  
     ><img src="./icons/filter-icoon.svg" alt="Filter" /></button>
     </header>
-    <BuddyList />
+    <BuddyList data={data = userData}/>
     {/* <main className={styles.filterContainer}>
       <section>
         <p className={styles.leadingText}>ik wil contact met...</p>
