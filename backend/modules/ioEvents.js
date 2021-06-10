8bd8ee06d0f00db010782e13fac2c5eab2fef7c6
sjagoori <sjagoori@users.noreply.github.com>
2021-06-10 23:12:21 +0200

    + data & consent flow

--- backend/modules/ioEvents.js
+++ backend/modules/ioEvents.js
@@ -1,33 +1,42 @@
-const messageCache = require('./messageCache')
-
-const tempTestVariable = {
-  "chat_id": [
-    {
-      "user_id": "user 1",
-      "message": "message",
-      "timestamp": 1623236809378,
-      "chat_id": "chat_id"
-    },
-    {
-      "user_id": "user 2",
-      "message": "message",
-      "timestamp": 1623236809378,
-      "chat_id": "chat_id"
-    }
-  ]
-}
+const messageCache = require('./messageCache'),
+  db = require("../modules/database"),
+  redis = require('../modules/messageCache');
 
 exports.eventHandler = async (client, server) => {
   client.on('message', message => {
     console.log(message);
-    client.broadcast.to(pair).emit('loadChatHistory', 'loadChatHistory')
+
+    db.updateOne('bloom', 'userdata',
+      {
+        user_id: message.consent.from.identifier,
+        chats: [
+          {
+            chat_id: message.chat_id,
+            from: {
+              identifier: message.from.identifier,
+              consent: true
+            },
+            to: {
+              identifier: message.to.identifier,
+              consent: false
+            }
+          }
+        ]
+      }
+    )
+
+    redis.setCache(message.chat_id, JSON.stringify(message))
+
+    // client.broadcast.to(message.chat_id).emit('loadChatHistory', 'loadChatHistory')
   })
 
   client.on('getPartners', partners => {
+    console.log("partners", partners)
+
     // * this will be the room name
     const pairData = Object.values(partners).sort((a, b) => a.user_id.localeCompare(b.user_id))
     const pair = pairData[0].user_id + pairData[1].user_id
-    console.log(pair)
+    console.log("pair", pair)
 
     // make a room
     client.join(pair)
@@ -35,21 +44,16 @@ exports.eventHandler = async (client, server) => {
     // TODO get partners from somewhere
     // * dont forget to broadcast these to the room instead
     // client.broadcast.to(pair).emit('setPartners', {
-    client.emit('setPartners',
-      // get these values from the getPartners event above
-      {
-        // load this from a db
-        from: {
-          identifier: "someone",
-          consent: false
-        },
-        to: {
-          identifier: 'someoneelse',
-          consent: true
-        }
-      }
-    )
-
-
+    client.emit('setPartners', {
+      from: {
+        identifier: pairData[0].user_id,
+        consent: true
+      },
+      to: {
+        identifier: pairData[1].user_id,
+        consent: false
+      },
+      chat_id: pair
+    })
   })
 }
