7fd2cb7cb73bd260da81bc74537539354e57fbcf
sjagoori <sjagoori@users.noreply.github.com>
2021-06-10 23:11:30 +0200

    + retreive consent objects
    Co-authored-by: Nathan <dewarian@users.noreply.github.com>

--- frontend/pages/chat.js
+++ frontend/pages/chat.js
@@ -3,55 +3,63 @@ import { useEffect, useState } from 'react'
 import { parseCookie } from "../helpers/parseCookie";
 
 export default function Chat() {
-  const socket = io('http://localhost:3001');
+  const socket = io('https://bloom.bloomingbooty.repl.co');
   const [messages, setMessages] = useState([])
   const [consent, setConsent] = useState({ from: { consent: false }, to: { consent: false } })
 
   useEffect(() => {
     let user_id_from_cookie = window.document.cookie ? JSON.parse(parseCookie(window.document.cookie).user).data.user_id : null
 
-    console.log(user_id_from_cookie);
-
-
     socket.emit('getPartners', {
       from: {
         user_id: user_id_from_cookie
-        // user_id: "a"
       },
       to: {
-        user_id: '$2b$10$SYfsPih9S/.FMC9E/FeWkuDT8.lkE..x7ewu.0t4OmGb60s25RoO.'
-        // user_id: query.userData.user_id
+        user_id: window.location.search.replace('?user_id=', "")
       }
     })
 
-    socket.on('setPartners', partner => { setConsent(partner) })
+    socket.on('setPartners', partner => {
+      setConsent({
+        from: {
+          consent: partner.from.consent,
+          identifier: decodeURIComponent(partner.from.identifier)
+        },
+        to: {
+          consent: partner.to.consent,
+          identifier: decodeURIComponent(partner.to.identifier)
+        },
+        chat_id: decodeURIComponent(partner.chat_id)
+      })
+    })
     socket.on('loadChatHistory', message => { setMessages(messages => [...messages, ...[message]]) })
   }, [])
 
-  console.log(consent);
+
+  console.log(consent)
 
   function handleChat(e) {
     e.preventDefault();
 
     socket.emit('message', {
-      from: 'me',
-      to: 'otherperson',
+      from: consent.from.identifier,
+      to: consent.to.identifier,
       consent: {
         from: {
-          identifier: 'me',
+          identifier: consent.from.identifier,
           consent: true
         },
         to: {
-          identifier: 'otherperson',
+          identifier: consent.to.identifier,
           consent: false
         },
       },
       message: e.target[1].value,
-      timestamp: +new Date
+      timestamp: +new Date,
+      chat_id: decodeURIComponent(consent.chat_id)
     })
   }
 
-  // console.log(messages);
 
   return (
     <aside>
@@ -66,12 +74,11 @@ export default function Chat() {
             })}
           </ol>
         </div>
-        {consent.from.consent && consent.to.consent
-          ? <> <input type="text" name="message" id="message" />
-            <button type="submit">Send</button></>
-          : <p>Yall shall not</p>}
+        <input type="text" name="message" id="message" />
+        <button type="submit">Send</button>
 
       </form>
     </aside>
   )
 }
+
