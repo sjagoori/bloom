c11ca51b50b2dcf188165d97b4a2dc51c94cc919
sjagoori <sjagoori@users.noreply.github.com>
2021-06-14 11:39:49 +0200

    + chat [rewrite from scratch]

--- frontend/pages/chat.js
+++ frontend/pages/chat.js
@@ -1,84 +1,106 @@
-import { io } from "socket.io-client";
-import { useEffect, useState } from 'react'
-import { parseCookie } from "../helpers/parseCookie";
+import { useRouter } from 'next/router';
+import { useEffect, useState, useRef } from 'react';
+import LoadingScreen from '@/components/loadingScreen/loadingScreen';
+import Header from '../components/header/Header'
+import Avatar from 'boring-avatars';
+import Link from 'next/link'
+import Badge from '@material-ui/core/Badge'
+import sortID from '../utils/sortID'
+import parseCookie from '@/utils/parseCookie';
 
 export default function Chat() {
-  const socket = io('https://bloom.bloomingbooty.repl.co');
-  const [messages, setMessages] = useState([])
-  const [consent, setConsent] = useState({ from: { consent: false }, to: { consent: false } })
+  const router = useRouter();
+  const { user } = router.query;
+  const [list, setList] = useState();
+  const [requests, setRequests] = useState();
+  const [view, setView] = useState()
 
-  useEffect(() => {
-    let user_id_from_cookie = window.document.cookie ? JSON.parse(parseCookie(window.document.cookie).user).data.user_id : null
+  useEffect(async () => {
+    let user_id_from_cookie = JSON.parse(parseCookie(window.document.cookie).user)
 
-    socket.emit('getPartners', {
-      from: {
-        user_id: user_id_from_cookie
-      },
-      to: {
-        user_id: window.location.search.replace('?user_id=', "")
-      }
+    const res = await fetch(`https://bloom.bloomingbooty.repl.co/getUserChat/${user_id_from_cookie}`)
+      .then(res => res.json())
+      .then(data => data)
+
+    let requestList = Object.values(res.list[0]).map((key, index) => {
+      let success = <UserCard data={key} key={index} link={sortID(user_id_from_cookie, key.user_id)} />
+
+      let empty = <div key={index}>
+        <h2>Requests</h2>
+        empty
+      </div>
+
+      return typeof res.list[0][0] == "boolean" ? empty : success
+
     })
 
-    socket.on('setPartners', partner => {
-      setConsent({
-        from: {
-          consent: partner.from.consent,
-          identifier: decodeURIComponent(partner.from.identifier)
-        },
-        to: {
-          consent: partner.to.consent,
-          identifier: decodeURIComponent(partner.to.identifier)
-        },
-        chat_id: decodeURIComponent(partner.chat_id)
-      })
+    let list = Object.values(res.requests[0]).map((key, index) => {
+      let success = <div key={index}>
+        {key.name}
+      </div>
+
+      let empty = <div key={index}>
+        <h2>Ongoing chats</h2>
+        empty
+      </div>
+
+      // TODO handle the backend type of the requestedList return
+      return typeof res.requests[0][0] == "boolean" ? empty : success
     })
-    socket.on('loadChatHistory', message => { setMessages(messages => [...messages, ...[message]]) })
+
+    console.log('user_id_from_cookie', user_id_from_cookie)
+    console.log('res.list[0]', res.list[0])
+    console.log('res.requests[0]', res.requests[0])
+
+    setList(list)
+    setRequests(requestList)
+    setView(list)
   }, [])
 
 
-  console.log(consent)
-
-  function handleChat(e) {
-    e.preventDefault();
-
-    socket.emit('message', {
-      from: consent.from.identifier,
-      to: consent.to.identifier,
-      consent: {
-        from: {
-          identifier: consent.from.identifier,
-          consent: true
-        },
-        to: {
-          identifier: consent.to.identifier,
-          consent: false
-        },
-      },
-      message: e.target[1].value,
-      timestamp: +new Date,
-      chat_id: decodeURIComponent(consent.chat_id)
-    })
-  }
-
+  console.log(view)
 
   return (
-    <aside>
-      <form onSubmit={handleChat}>
-        <div>
-          <fieldset>Chat</fieldset>
-          <ol>
-            {messages.map((key, index) => {
-              return <li key={index}>
-                {key.message}
-              </li>
-            })}
-          </ol>
-        </div>
-        <input type="text" name="message" id="message" />
-        <button type="submit">Send</button>
+    <>
+      <Header name="Chats" isBlogs={false}>
+        <Badge badgeContent={0} color="error">
+          <button onClick={() => view == list ? setView(requests) : setView(list)}>Requests</button>
+        </Badge>
+      </Header>
 
-      </form>
-    </aside>
+      {view ? view : <LoadingScreen />}
+    </>
   )
 }
 
+
+const UserCard = (props) => (
+  <Link href={`/chatbox/${props.link}`}>
+    <a>
+      <Avatar
+        size={60}
+        name={props.data.name}
+        variant="beam"
+        colors={["#FEE89E", "#F07A06", "#F07903", "#3CB2FF", "#CE6F88"]}
+      ></Avatar>
+      <span>{props.data.name}</span>
+    </a>
+  </Link>
+)
+
+// Chat.getInitialProps = async (ctx) => {
+//   const another = await ctx.req.headers.cookie;
+
+//   if (typeof another === 'undefined') {
+//     // On server
+//     if (typeof window === 'undefined') {
+//       ctx.res.writeHead(302, { location: '/' })
+//       ctx.res.end()
+//     } else {
+//       // On client
+//       Router.push('/')
+//     }
+//   }
+
+//   return {}
+// }
