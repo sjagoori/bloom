7a00e7fd5ff1c98baf664dd899b810801f586379
sjagoori <sjagoori@users.noreply.github.com>
2021-06-14 11:36:52 +0200

    - unused files [rewrite from scratch]

--- frontend/pages/api/[theme].js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-export default function themeHandler(req, res) {
-    try {
-    return res.status(200).json(JSON.parse(fs.readFileSync(`./pages/api/_files/${req.query.theme}.json`, 'utf-8')))
-  } catch (error) {
-    return res.status(400).json({msg: "*instense confusion stare*"})
-  }
-}
