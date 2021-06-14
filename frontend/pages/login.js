f988af50b9e4c6411be6c650c818862141e2404c
sjagoori <sjagoori@users.noreply.github.com>
2021-06-14 11:42:57 +0200

    ~ updated paths [rewrite from scratch]

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { parseCookie } from "../helpers/parseCookie";
+import { parseCookie } from "../utils/parseCookie";
 import Link from "next/link";
 import { useCookies } from "react-cookie";
 import styles from "../styles/Login.module.css";
@@ -11,10 +11,10 @@ export default function Login() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (window.document.cookie)
-      JSON.parse(parseCookie(window.document.cookie).user).data.user_id != ''
-        ? router.push({ pathname: "dashboard" })
-        : router.push({ pathname: "/" })
+    // if (window.document.cookie)
+    //   JSON.parse(parseCookie(window.document.cookie).user).data.user_id != ''
+    //     ? router.push({ pathname: "dashboard" })
+    //     : router.push({ pathname: "/" })
   });
 
   async function handleForm(e) {
@@ -35,13 +35,13 @@ export default function Login() {
       .then((res) => res.json())
       .then((data) => {
         data.status === 200
-          ? (setCookie("user", JSON.stringify(data), {
+          ? (setCookie("user", JSON.stringify(data.data.user_id), {
             path: "/",
             maxAge: 3600, // Expires after 1hr
             sameSite: true,
           }),
             router.push({
-              pathname: "dashboard",
+              pathname: "blog",
             }))
           : setError("Email or password incorrect");
       });
