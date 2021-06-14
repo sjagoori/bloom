a9ad9327468a2b67064f7969e68d2e3987563e8b
sjagoori <sjagoori@users.noreply.github.com>
2021-06-14 11:38:46 +0200

    + handle loading [rewrite from scratch]

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,36 @@
-import { CookiesProvider } from "react-cookie";
-import "../styles/globals.css";
+import '../styles/globals.css'
+import Navbar from '../components/navbar/Navbar'
+import LoadingScreen from '../components/loadingScreen/loadingScreen'
+import React, { useState } from 'react';
+import Router from 'next/router';
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const noNav = ['/', '/login']
+
+  Router.onRouteChangeStart = () => {
+    setLoading(true)
+  }
+
+  Router.onRouteChangeComplete = (e) => {
+    setLoading(false)
+  }
+
   return (
-    <CookiesProvider>
-      <Component {...pageProps} />
-    </CookiesProvider>
-  );
+    loading
+      ? <>
+        <LoadingScreen />
+        <Navbar />
+      </>
+      : <>
+        <Component {...pageProps} ></Component>
+        {noNav.includes(router.pathname) ? null : <Navbar />}
+      </>
+  )
 }
 
 export default MyApp;
+
